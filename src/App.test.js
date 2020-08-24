39a2dace0ed1e472934d7f84cae1a5ec7c7a7424
Dilantha Wijayarathne <dilantha.prasanjith111@gmail.com>
2020-08-24 17:42:07 +0530

    add enzyme testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,32 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { shallow, mount, render, act } from 'enzyme';
 import { getMetalGenres } from './services/music-genres';
 import App from './App';
 
 jest.mock('./services/music-genres');
 
 describe("App component", () => {
+  const genreList = [
+    "Gothic Metal",
+    "Thrash Metal",
+    "Heavy Metal"
+  ];
+
+  beforeEach(() => {
+    getMetalGenres.mockResolvedValue(genreList);
+  });
+
   afterEach(() => {
     jest.resetAllMocks();
   });
-  
-  test("user can click the fetch button and get a list of metal music genres", async () => {
-    const genreList = [
-      "Gothic Metal",
-      "Thrash Metal",
-      "Heavy Metal"
-    ];
-    getMetalGenres.mockResolvedValueOnce(genreList);
 
-    const { getByText, findByText, getAllByRole } = render(<App />);
-    expect(getByText(/Metal Music Genres/i)).toBeVisible();
-
-    fireEvent.click(getByText(/Fetch Some Metal Music/i));
-    expect(await findByText(/loading/i)).toBeVisible();
-    expect(getMetalGenres).toHaveBeenCalledTimes(1);
-
-    const genreElsText = getAllByRole('listitem').map(el => el.textContent.trim());
-    expect(genreElsText).toEqual(genreList);
+  it('should render without throwing an error', () => {
+    expect(shallow(<App />).contains(<h1> Metal Music Genres </h1>)).toBe(true);
   });
 
-  test("user can see an error message if failed to get a list of metal music genres", async () => {
-    getMetalGenres.mockRejectedValueOnce("Failed to fetch");
-
-    const { getByText, findByText } = render(<App />);
-
-    fireEvent.click(getByText(/Fetch Some Metal Music/i));
-    expect(await findByText(/loading/i)).toBeVisible();
+  it('calls the getMetalGenres properly', async () => {
+    const wrapper = mount(<App/>);
+    wrapper.find('.btn-container button').simulate('click');
     expect(getMetalGenres).toHaveBeenCalledTimes(1);
-
-    expect(await findByText(/failed to fetch/i)).toBeVisible();
   });
 });
