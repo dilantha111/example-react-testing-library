1cf8f9ba7f58ca46583c4ca36769bacd61a8a466
Dilantha Wijayarathne <dilantha.prasanjith111@gmail.com>
2020-08-24 11:07:30 +0530

    add the bad way of testing it

--- src/App.test.js
+++ src/App.test.js
@@ -1,42 +1,61 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor, waitForElement, wait, waitForDomChange } from '@testing-library/react';
 import { getMetalGenres } from './services/music-genres';
 import App from './App';
 
 jest.mock('./services/music-genres');
 
 describe("App component", () => {
+  let utils;
+  const genreList = [
+    "Gothic Metal",
+    "Thrash Metal",
+    "Heavy Metal"
+  ];
+
+  beforeEach(() => {
+    utils = render(<App />);
+    getMetalGenres.mockResolvedValue(genreList);
+  });
+
   afterEach(() => {
     jest.resetAllMocks();
   });
-  
-  test("user can click the fetch button and get a list of metal music genres", async () => {
-    const genreList = [
-      "Gothic Metal",
-      "Thrash Metal",
-      "Heavy Metal"
-    ];
-    getMetalGenres.mockResolvedValueOnce(genreList);
 
-    const { getByText, findByText, getAllByRole } = render(<App />);
+  it("shows header for the metal genres", () => {
+    const { getByText } = utils;
     expect(getByText(/Metal Music Genres/i)).toBeVisible();
-
-    fireEvent.click(getByText(/Fetch Some Metal Music/i));
-    expect(await findByText(/loading/i)).toBeVisible();
-    expect(getMetalGenres).toHaveBeenCalledTimes(1);
-
-    const topicElsText = getAllByRole('listitem').map(el => el.textContent.trim());
-    expect(topicElsText).toEqual(genreList);
   });
 
-  test("user can see an error message if failed to get a list of metal music genres", async () => {
-    getMetalGenres.mockRejectedValueOnce("Failed to fetch");
+  it("shows button to fetch metal genres", () => {
+    const { getByText } = utils;
+    expect(getByText(/Fetch Some Metal Music/i)).toBeVisible();
+  });
 
-    const { getByText, findByText } = render(<App />);
-
+  it("can click on the fetch button and will show a loading text", async () => {
+    const { getByText, findByText } = utils;
     fireEvent.click(getByText(/Fetch Some Metal Music/i));
     expect(await findByText(/loading/i)).toBeVisible();
+  });
+
+  it("will call the getMetalGenres function properly when fetch button is clicked", async () => {
+    const { getByText } = utils;
+    fireEvent.click(getByText(/Fetch Some Metal Music/i));
     expect(getMetalGenres).toHaveBeenCalledTimes(1);
+  });
+
+  it("will show a list of metal genres when fetch button was clicked", async () => {
+    const { getByText, findByText, getAllByRole } = utils;
+    fireEvent.click(getByText(/Fetch Some Metal Music/i));
+    expect(await findByText(/Gothic Metal/i)).toBeVisible();
+    const listElsText = getAllByRole('listitem').map(el => el.textContent.trim());
+    expect(listElsText).toEqual(genreList);
+  });
+
+  it("will show an error message if failed to fetch a list of metal genres", async () => {
+    getMetalGenres.mockRejectedValueOnce("Failed to fetch");
+    const { getByText, findByText, getAllByRole } = utils;
+    fireEvent.click(getByText(/Fetch Some Metal Music/i));
 
     expect(await findByText(/failed to fetch/i)).toBeVisible();
   });
